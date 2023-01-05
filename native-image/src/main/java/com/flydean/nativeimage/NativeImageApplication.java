51bb7710e5848b5871dd6afba40c457b09431dea
wayne <wayne@ouba.local>
2023-01-05 17:18:38 +0800

    在spring boot3中使用native image

---
+++ native-image/src/main/java/com/flydean/nativeimage/NativeImageApplication.java
@@ -0,0 +1,13 @@
+package com.flydean.nativeimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeImageApplication.class, args);
+    }
+
+}
---
+++ native-image/src/test/java/com/flydean/nativeimage/NativeImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flydean.nativeimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
